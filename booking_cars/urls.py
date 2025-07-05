7cfd12282f8dfd21b60390f0c321e429f09f3850
unknown <antonsibra836@gmail.com>
2025-07-05 18:47:18 +0200

    05 07 2025

--- booking_cars/urls.py
+++ booking_cars/urls.py
@@ -2,6 +2,9 @@ from django.urls import path,include
 from booking_cars import views
 
 urlpatterns = [
-    path('',views.main_filter,name='cars')
+    path('',views.main_filter,name='cars'),
+    path('/car/<int:id>/',views.car_details,name='Car details'),
+    path('/car/<int:id>/book',views.book_car,name='Book car')
 ]
+
  
--- booking_system/urls.py
+++ booking_system/urls.py
@@ -21,6 +21,7 @@ from booking_system import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('booking_cars.urls'))
+    path('',include('booking_cars.urls')),
+    path('accounts/',include('django.contrib.auth.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
  
